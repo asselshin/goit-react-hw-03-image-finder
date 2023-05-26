f8940c932c19b62c14dc2cedb94158df91185331
Assel Shingissova <asselshin@gmail.com>
2023-05-26 21:23:28 +0600

    update after check

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,44 +1,46 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
-import { fetchData } from "api";
-import Searchbar from "./Searchbar/Searchbar";
-import ImageGallery from "./ImageGallery/ImageGallery";
-import Button from "./Button/Button";
-import Loader from "./Loader/Loader";
-import Modal from "./Modal/Modal";
+import { fetchData } from 'api';
+import Searchbar from './Searchbar/Searchbar';
+import ImageGallery from './ImageGallery/ImageGallery';
+import Button from './Button/Button';
+import Loader from './Loader/Loader';
+import Modal from './Modal/Modal';
 
 class App extends Component {
   state = {
     searchInput: '',
-    images: [], 
-    page: 1,    
+    images: [],
+    page: 1,
     loading: false,
     error: null,
     showBtn: false,
     showModal: false,
     modalImg: '',
-    modalAlt:'',
-  } 
+    modalAlt: '',
+  };
 
-  async componentDidUpdate (prevProps, prevState) {
-    if ((prevState.searchInput !== this.state.searchInput) || (prevState.page !== this.state.page)) {
+  async componentDidUpdate(prevProps, prevState) {
+    if (
+      prevState.searchInput !== this.state.searchInput ||
+      prevState.page !== this.state.page
+    ) {
       try {
         this.setState({ loading: true });
-       
-        const fetchedSearch = await fetchData(this.state.searchInput, this.state.page)        
+
+        const fetchedSearch = await fetchData(
+          this.state.searchInput,
+          this.state.page
+        );
+        const fetchedPages = Math.floor(fetchedSearch.hits.length / 12);
+
         this.setState(prevState => ({
           images: [...prevState.images, ...fetchedSearch.hits],
+          showBtn: fetchedPages ? true : false,
         }));
-       
-        const fetchedPages = Math.floor(fetchedSearch.hits.length / 12);
-        if (fetchedPages) {
-          this.setState ({showBtn:true})
-        } else {
-          this.setState({ showBtn:false})
-        }
-      
+
       } catch (error) {
-        this.setState({error: 'Something wrong'})
+        this.setState({ error: 'Something wrong' });
       
       } finally {
         this.setState({ loading: false });
@@ -46,14 +48,14 @@ class App extends Component {
     }
   }
 
-    toggleModal = () => {
+  toggleModal = () => {
     this.setState(({ showModal }) => ({
       showModal: !showModal,
-    }))
+    }));
   };
 
-  formSubmitHandler = (newSearchInput) => {
-    this.setState ({...newSearchInput, images: [], page: 1 })
+  formSubmitHandler = newSearchInput => {
+    this.setState({ ...newSearchInput, images: [], page: 1 });
   };
 
   handleLoadMore = () => {
@@ -67,23 +69,31 @@ class App extends Component {
       modalImg: imgUrl,
       modalAlt: alt,
       showModal: true,
+    });
+  };
 
-    })
-  }
-
-  render() {  
-    const { loading,error,images, showBtn, showModal, modalImg, modalAlt } = this.state;
+  render() {
+    const { loading, error, images, showBtn, showModal, modalImg, modalAlt } =
+      this.state;
     return (
       <div className="App">
         <Searchbar onSubmit={this.formSubmitHandler} />
-        {loading && <Loader/>}
-        {error && <div>{error}</div>} 
-        {images.length > 1 && <ImageGallery searchedArray={images} imgClick={this.handleImgClick} />}
+        {loading && <Loader />}
+        {error && <div>{error}</div>}
+        {images.length > 1 && (
+          <ImageGallery searchedArray={images} imgClick={this.handleImgClick} />
+        )}
         {showBtn && <Button loadMore={() => this.handleLoadMore()} />}
-        {showModal && (<Modal onClose={this.toggleModal} modalSrc={modalImg} modalAlt = {modalAlt} />)} 
+        {showModal && (
+          <Modal
+            onClose={this.toggleModal}
+            modalSrc={modalImg}
+            modalAlt={modalAlt}
+          />
+        )}
       </div>
     );
   }
-};
+}
 
 export default App;
